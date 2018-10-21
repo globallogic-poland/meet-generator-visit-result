d0a045e635f6c1778e57cb7522c6ec9aa8c1c39c
hhh <milosz.mazurek@gmail.com>
2018-10-21 18:08:13 +0200

    Corrections after code rewiv - changed chanel names and port

--- src/main/java/meet/generator/visit/result/adapters/VisitResultGenerator.java
+++ src/main/java/meet/generator/visit/result/adapters/VisitResultGenerator.java
@@ -3,6 +3,7 @@ package meet.generator.visit.result.adapters;
 
 import meet.generator.visit.result.model.Drug;
 import meet.generator.visit.result.model.Visit;
+import meet.generator.visit.result.ports.VisitResultBinding;
 import meet.generator.visit.result.services.VisitResultGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -12,7 +13,11 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import reactor.core.publisher.Flux;
 
-@EnableBinding(Processor.class)
+import static meet.generator.visit.result.ports.VisitResultBinding.VISITS_AFTER_APPOINTMANT;
+import static meet.generator.visit.result.ports.VisitResultBinding.VISITS_BEFORE_APPOINTMANT;
+import static org.springframework.cloud.stream.messaging.Source.OUTPUT;
+
+@EnableBinding(VisitResultBinding.class)
 public class VisitResultGenerator {
 
     private VisitResultGeneratorService visitResultGeneratorService;
@@ -22,8 +27,8 @@ public class VisitResultGenerator {
     }
 
     @StreamListener
-    @Output(Processor.OUTPUT)
-    public Flux<Visit> generateVisitResult(@Input(Processor.INPUT) Flux<Visit> inbound) {
+    @Output(VISITS_AFTER_APPOINTMANT)
+    public Flux<Visit> generateVisitResult(@Input(VISITS_BEFORE_APPOINTMANT) Flux<Visit> inbound) {
         return inbound.
                 log()
                 .map(visitResultGeneratorService::generate)
