a3cd193398f2f3ea9ae69e158128f78484ff06cc
Robert Gonciarz <gonciarz@gmail.com>
2018-10-24 22:02:49 +0200

    cleanup

--- src/main/java/meet/generator/visit/result/ports/VisitResultBinding.java
+++ src/main/java/meet/generator/visit/result/ports/VisitResultBinding.java
@@ -3,20 +3,15 @@ package meet.generator.visit.result.ports;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 public interface VisitResultBinding {
 
     String VISITS_BEFORE_APPOINTMENT = "visits-before-appointment";
     String VISITS_AFTER_APPOINTMENT = "visits-after-appointment";
-    String OUTCOMMING_VISITS = "outcomming-visits";
 
     @Input(VISITS_BEFORE_APPOINTMENT)
     MessageChannel visitsBeforeAppontment();
 
     @Output(VISITS_AFTER_APPOINTMENT)
     MessageChannel visitsAfterAppontment();
-
-    @Input(OUTCOMMING_VISITS)
-    SubscribableChannel outcommingVisits();
 }
